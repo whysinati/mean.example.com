887447879d7d7e8db3cc26492a6dea37b540664e
Christa Fuhrhop <christa.fuhrhop@gmail.com>
2019-04-24 18:30:21 -0500

    finished out users stuff

--- src/js/users.app.js
+++ src/js/users.app.js
@@ -1,6 +1,6 @@
-var usersApp = (function() {
+var usersApp = (function () {
 
-  function viewUsers(){
+  function viewUsers() {
 
     let uri = `${window.location.origin}/api/users`;
     let xhr = new XMLHttpRequest();
@@ -13,7 +13,7 @@ var usersApp = (function() {
 
     xhr.send();
 
-    xhr.onload = function(){
+    xhr.onload = function () {
       let app = document.getElementById('app');
       let data = JSON.parse(xhr.response);
       let users = data.users;
@@ -22,95 +22,95 @@ var usersApp = (function() {
 
       //Loop each user record into it's own HTML table row, each user should
       //have a link a user view
-      for (let i=0; i<users.length; i++) {
+      for (let i = 0; i < users.length; i++) {
         rows = rows + `<tr>
           <td>
             <a href="#view-${users[i]['_id']}">${users[i]['last_name']}, ${users[i]['first_name']}</a>
           </td>
           <td>${users[i]['username']}</td>
           <td>${users[i]['email']}</td>
-        </tr>`;
+            </tr>`;
       }
 
       //Create a users panel, add a table to the panel, inject the rows into the
       //table
       table = `<div class="card">
-        <div class="card-header clearfix">
-          <h2 class="h3 float-left">Users</h2>
-          <div class="float-right">
-            <a href="#create" class="btn btn-primary">New User</a>
-          </div>
-        </div>
-        <div class="table-responsive">
-          <table class="table table-striped table-hover table-bordered">
-            <thead>
-              <tr>
-                <td>Name</td>
-                <td>Username</td>
-                <td>Email</td>
-              </tr>
-            </thead>
-            <tbody>${rows}</tbody>
-          </table>
-        </div>
-      </div>`;
+            <div class="card-header clearfix">
+            <h2 class="h3 float-left">Users</h2>
+            <div class="float-right">
+                <a href="#create" class="btn btn-primary">New User</a>
+            </div>
+            </div>
+            <div class="table-responsive">
+            <table class="table table-striped table-hover table-bordered">
+                <thead>
+                <tr>
+                    <td>Name</td>
+                    <td>Username</td>
+                    <td>Email</td>
+                </tr>
+                </thead>
+                <tbody>${rows}</tbody>
+            </table>
+            </div>
+            </div>`;
 
       //Append the HTML to the #app
       app.innerHTML = table;
     }
   }
 
-  function createUser(){
+  function createUser() {
     var app = document.getElementById('app');
 
-    var form =  `
-        <div class="card">
-          <div class="card-header clearfix">
-            <h2 class="h3 float-left">Create a New User</h2>
-            <div class="float-right">
-              <a href="#" class="btn btn-primary">Cancel</a>
-            </div>
-          </div>
-          <div class="card-body">
-            <form id="createUser" class="card-body">
-              <div id="formMsg" class="alert alert-danger text-center">Your form has errors</div>
-
-              <div class="row">
-                <div class="form-group col-md-6">
-                  <label for="first_name">First Name</label>
-                  <input type="text" id="first_name" name="first_name" class="form-control" required>
-                </div>
-
-                <div class="form-group col-md-6">
-                  <label for="last_name">Last Name</label>
-                  <input type="text" id="last_name" name="last_name" class="form-control" required>
-                </div>
-              </div>
-
-              <div class="row">
-                <div class="form-group col-md-6">
-                  <label for="username">Username</label>
-                  <input type="text" id="username" name="username" class="form-control" required>
-                </div>
-
-                <div class="form-group col-md-6">
-                  <label for="email">Email</label>
-                  <input type="email" id="email" name="email" class="form-control" required>
-                </div>
-              </div>
-
-              <div class="text-right">
-                <input type="submit" value="Submit" class="btn btn-lg btn-primary btn-sm-block">
-              </div>
-            </form>
-          </div>
+    var form = `
+    <div class="card">
+        <div class="card-header clearfix">
+        <h2 class="h3 float-left">Create a New User</h2>
+        <div class="float-right">
+            <a href="#" class="btn btn-primary">Cancel</a>
         </div>
-    `;
+        </div>
+        <div class="card-body">
+        <form id="createUser" class="card-body">
+            <div id="formMsg" class="alert alert-danger text-center">Your form has errors</div>
 
-    app.innerHTML=form;
+            <div class="row">
+            <div class="form-group col-md-6">
+                <label for="first_name">First Name</label>
+                <input type="text" id="first_name" name="first_name" class="form-control" required>
+            </div>
+
+            <div class="form-group col-md-6">
+                <label for="last_name">Last Name</label>
+                <input type="text" id="last_name" name="last_name" class="form-control" required>
+            </div>
+            </div>
+
+            <div class="row">
+            <div class="form-group col-md-6">
+                <label for="username">Username</label>
+                <input type="text" id="username" name="username" class="form-control" required>
+            </div>
+
+            <div class="form-group col-md-6">
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" class="form-control" required>
+            </div>
+            </div>
+
+            <div class="text-right">
+            <input type="submit" value="Submit" class="btn btn-lg btn-primary btn-sm-block">
+            </div>
+        </form>
+        </div>
+    </div>
+`;
+
+    app.innerHTML = form;
   }
 
-  function viewUser(id){
+  function viewUser(id) {
 
     let uri = `${window.location.origin}/api/users/${id}`;
     let xhr = new XMLHttpRequest();
@@ -123,29 +123,29 @@ var usersApp = (function() {
 
     xhr.send();
 
-    xhr.onload = function(){
+    xhr.onload = function () {
       let app = document.getElementById('app');
       let data = JSON.parse(xhr.response);
       let card = '';
 
       card = `<div class="card">
-        <div class="card-header clearfix">
-          <h2 class="h3 float-left">${data.user.first_name} ${data.user.last_name}</h2>
-          <div class="float-right">
-            <a href="#edit-${data.user._id}" class="btn btn-primary">Edit</a>
-          </div>
+    <div class="card-header clearfix">
+        <h2 class="h3 float-left">${data.user.first_name} ${data.user.last_name}</h2>
+        <div class="float-right">
+        <a href="#edit-${data.user._id}" class="btn btn-primary">Edit</a>
         </div>
-        <div class="card-body">
-          <div>${data.user.username}</div>
-          <div>${data.user.email}</div>
-        </div>
-      </div>`;
+    </div>
+    <div class="card-body">
+        <div>${data.user.username}</div>
+        <div>${data.user.email}</div>
+    </div>
+    </div>`;
 
       app.innerHTML = card;
     }
   }
 
-  function editUser(id){
+  function editUser(id) {
 
     let uri = `${window.location.origin}/api/users/${id}`;
     let xhr = new XMLHttpRequest();
@@ -158,66 +158,66 @@ var usersApp = (function() {
 
     xhr.send();
 
-    xhr.onload = function(){
+    xhr.onload = function () {
       let app = document.getElementById('app');
       let data = JSON.parse(xhr.response);
 
-      var form =  `
-        <div class="card">
-          <div class="card-header clearfix">
-            <h2 class="h3 float-left">Edit</h2>
-            <div class="float-right">
-              <a href="#" class="btn btn-primary">Cancel</a>
+      var form = `
+            <div class="card">
+            <div class="card-header clearfix">
+                <h2 class="h3 float-left">Edit</h2>
+                <div class="float-right">
+                <a href="#" class="btn btn-primary">Cancel</a>
+                </div>
             </div>
-          </div>
-          <div class="card-body">
-            <form id="editUser" class="card-body">
-              <input type="hidden" id="_id" name="_id" value="${data.user._id}">
-              <div id="formMsg" class="alert alert-danger text-center">Your form has errors</div>
-
-              <div class="row">
-                <div class="form-group col-md-6">
-                  <label for="first_name">First Name</label>
-                  <input type="text" id="first_name" name="first_name" class="form-control" value="${data.user.first_name}" required>
+            <div class="card-body">
+                <form id="editUser" class="card-body">
+                <input type="hidden" id="_id" name="_id" value="${data.user._id}">
+                <div id="formMsg" class="alert alert-danger text-center">Your form has errors</div>
+        
+                <div class="row">
+                    <div class="form-group col-md-6">
+                    <label for="first_name">First Name</label>
+                    <input type="text" id="first_name" name="first_name" class="form-control" value="${data.user.first_name}" required>
+                    </div>
+        
+                    <div class="form-group col-md-6">
+                    <label for="last_name">Last Name</label>
+                    <input type="text" id="last_name" name="last_name" class="form-control" value="${data.user.last_name}" required>
+                    </div>
                 </div>
-
-                <div class="form-group col-md-6">
-                  <label for="last_name">Last Name</label>
-                  <input type="text" id="last_name" name="last_name" class="form-control" value="${data.user.last_name}" required>
+        
+                <div class="row">
+                    <div class="form-group col-md-6">
+                    <label for="username">Username</label>
+                    <input type="text" id="username" name="username" class="form-control" value="${data.user.username}" required>
+                    </div>
+        
+                    <div class="form-group col-md-6">
+                    <label for="email">Email</label>
+                    <input type="email" id="email" name="email" class="form-control" value="${data.user.email}" required>
+                    </div>
                 </div>
-              </div>
-
-              <div class="row">
-                <div class="form-group col-md-6">
-                  <label for="username">Username</label>
-                  <input type="text" id="username" name="username" class="form-control" value="${data.user.username}" required>
+        
+                <div class="text-right">
+                    <input type="submit" value="Submit" class="btn btn-lg btn-primary btn-sm-block">
                 </div>
-
-                <div class="form-group col-md-6">
-                  <label for="email">Email</label>
-                  <input type="email" id="email" name="email" class="form-control" value="${data.user.email}" required>
+                </form>
                 </div>
-              </div>
+            </div>
+            <div>
+            <a href="#delete-${data.user._id}" class="text-danger">Delete</a>
+            </div>
+            `;
 
-              <div class="text-right">
-                <input type="submit" value="Submit" class="btn btn-lg btn-primary btn-sm-block">
-              </div>
-            </form>
-          </div>
-        </div>
-        <div>
-          <a href="#delete-${data.user._id}" class="text-danger">Delete</a>
-        </div>
-      `;
-
-      app.innerHTML=form;
+      app.innerHTML = form;
       processRequest('editUser', '/api/users', 'PUT');
     }
   }
 
-  function processRequest(formId, url, method){
+  function processRequest(formId, url, method) {
     let form = document.getElementById(formId);
-    form.addEventListener('submit', function(e){
+    form.addEventListener('submit', function (e) {
       e.preventDefault();
 
       let formData = new FormData(form);
@@ -231,26 +231,73 @@ var usersApp = (function() {
       );
 
       let object = {};
-      formData.forEach(function(value, key){
-        object[key]=value;
+      formData.forEach(function (value, key) {
+        object[key] = value;
       });
 
       xhr.send(JSON.stringify(object));
-      xhr.onload = function(){
+      xhr.onload = function () {
         let data = JSON.parse(xhr.response);
-        if(data.success===true){
+        if (data.success === true) {
           window.location.href = '/';
-        }else{
-          document.getElementById('formMsg').style.display='block';
+        } else {
+          document.getElementById('formMsg').style.display = 'block';
         }
       }
     });
   }
-  function deleteView(id){
+
+  function deleteView(id) {
 
     let uri = `${window.location.origin}/api/users/${id}`;
     let xhr = new XMLHttpRequest();
     xhr.open('GET', uri);
+
+    xhr.setRequestHeader(
+      'Content-Type',
+      'application/json; charset=UTF-8'
+    );
+
+    xhr.send();
+
+    xhr.onload = function () {
+      let app = document.getElementById('app');
+      let data = JSON.parse(xhr.response);
+      let card = '';
+
+      card = `<div class="card bg-transparent border-danger text-danger bg-danger">
+            <div class="card-header bg-transparent border-danger">
+              <h2 class="h3 text-center">Your About to Delete a User</h2>
+            </div>
+            <div class="card-body text-center">
+              <div>
+                Are you sure you want to delete
+                <strong>${data.user.first_name} ${data.user.last_name}</strong>
+              </div>
+      
+              <div>Username: <strong>${data.user.username}</strong></div>
+              <div>Email: <strong>${data.user.email}</strong></div>
+      
+              <div class="text-center">
+                <br>
+                <a onclick="usersApp.deleteUser('${data.user._id}');" class="btn btn-lg btn-danger text-white">
+                Yes delete ${data.user.username}</a>
+              </div>
+
+
+      
+            </div>
+          </div>`;
+
+      app.innerHTML = card;
+    }
+  }
+
+  function deleteUser(id){
+
+    let uri = `${window.location.origin}/api/users/${id}`;
+    let xhr = new XMLHttpRequest();
+    xhr.open('DELETE', uri);
   
     xhr.setRequestHeader(
       'Content-Type',
@@ -260,42 +307,24 @@ var usersApp = (function() {
     xhr.send();
   
     xhr.onload = function(){
-      let app = document.getElementById('app');
       let data = JSON.parse(xhr.response);
-      let card = '';
+      if(data.success === true){
+        window.location.hash = '#';
+      }else{
+        alert('Unknown error, the user could not be deleted');
+      }
   
-      card = `<div class="card bg-transparent border-danger text-danger bg-danger">
-        <div class="card-header bg-transparent border-danger">
-          <h2 class="h3 text-center">Your About to Delete a User</h2>
-        </div>
-        <div class="card-body text-center">
-          <div>
-            Are you sure you want to delete
-            <strong>${data.user.first_name} ${data.user.last_name}</strong>
-          </div>
-  
-          <div>Username: <strong>${data.user.username}</strong></div>
-          <div>Email: <strong>${data.user.email}</strong></div>
-  
-          <div class="text-center">
-            <br>
-            <a class="btn btn-lg btn-danger text-white">
-              Yes delete ${data.user.username}
-            </a>
-          </div>
-  
-        </div>
-      </div>`;
-  
-      app.innerHTML = card;
     }
+  
   }
+
   return {
-    load: function(){
+    load: function () {
+      viewUsers();
       let hash = window.location.hash;
       let hashArray = hash.split('-');
 
-      switch(hashArray[0]){
+      switch (hashArray[0]) {
         case '#create':
           createUser();
           processRequest('createUser', '/api/users', 'POST');
@@ -310,16 +339,23 @@ var usersApp = (function() {
           break;
 
         case '#delete':
-          deleteView(hashArray[1]);
-          break;
+        deleteView(hashArray[1]);
+        break;
 
         default:
           viewUsers();
           break;
       }
+    },
+        deleteUser: function(id){
+        deleteUser(id);
+      } 
     }
-  }
 
 })();
 
 usersApp.load();
+
+window.addEventListener("hashchange", function () {
+    usersApp.load();
+});
