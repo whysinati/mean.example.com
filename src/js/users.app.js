7f7f94d27226fb57f8e2546cac81c4669b4b53ba
Christa Fuhrhop <christa.fuhrhop@gmail.com>
2019-04-24 14:45:23 -0500

    hash edit page

--- src/js/users.app.js
+++ src/js/users.app.js
@@ -143,6 +143,26 @@ var usersApp = (function() {
       app.innerHTML = card;
     }
   }
+
+  function editUser(id){
+
+    let uri = `${window.location.origin}/api/users/${id}`;
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', uri);
+  
+    xhr.setRequestHeader(
+      'Content-Type',
+      'application/json; charset=UTF-8'
+    );
+  
+    xhr.send();
+  
+    xhr.onload = function(){
+      let data = JSON.parse(xhr.response);
+      console.log(data);
+    }
+  }
+
   function postRequest(formId, url){
     let form = document.getElementById(formId);
     form.addEventListener('submit', function(e){
@@ -190,7 +210,7 @@ var usersApp = (function() {
           break;
     
         case '#edit':
-          console.log('EDIT');
+          editUser(hashArray[1]);
           break;
     
         case '#delete':
