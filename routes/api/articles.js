a44aba71f78478cdbd50c814afe32e2b043936ed
Christa Fuhrhop <christa.fuhrhop@gmail.com>
2019-04-24 20:56:56 -0500

    functionality to retrieve a list of all articles

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var Articles = require('../../models/articles');
 
 router.get('/', function(req, res, next) {
-  res.json({success: true});
+    Articles.find({},function(err, articles){
+        if(err){
+         return res.json({'success':false, 'error': err});
+       }
+        return res.json({'success':true, 'articles': articles});
+      });
 });
 
 module.exports = router;
