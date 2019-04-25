e0332fd91132e95b61a5b532a5de77cbae14f65c
Christa Fuhrhop <christa.fuhrhop@gmail.com>
2019-04-24 20:24:53 -0500

    implemented a route for api/articles (in app.js)

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var apiUsersRouter = require('./routes/api/users');
 var apiAuthRouter = require('./routes/api/auth');
+var apiArticlesRouter = require('./routes/api/articles');
 var app = express();
 
 // view engine setup
@@ -130,6 +131,7 @@ app.use('/api/users', apiUsersRouter);
 app.use('/api/auth', apiAuthRouter);
 app.use('/auth', authRouter);
 app.use('/users', usersRouter);
+app.use('/api/articles', apiArticlesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
